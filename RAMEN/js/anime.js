f6b8dc577fad1c4abbb6ea228e50a0e488ad9b82
Johnley01 <122706442+Johnley01@users.noreply.github.com>
2025-02-13 11:59:28 -0500

    Add files via upload

---
+++ RAMEN/js/anime.js
@@ -0,0 +1,46 @@
+
+function show_menu() {
+    let menu = document.querySelector("#nav1 ul");
+    menu.classList.toggle("menu_active");
+}
+
+
+
+
+
+
+/*
+
+function nutri() {
+    if (document.getElementById("cal").style.display == "block") {
+        document.getElementById("cal").style.display = "none";
+    }
+    else {
+        document.getElementById("cal").style.display = "block";
+    }
+
+}
+
+function show_menu() {
+    if (document.getElementById("nav1").style.display == "block") {
+        document.getElementById("nav1").style.display = "none";
+    }
+    else {
+        document.getElementById("nav1").style.display = "block";
+    }
+
+}
+
+
+
+
+
+
+function sending() {
+    if (document.getElementById("txtbox").style.display == "block") {
+        document.getElementById("txtbox").style.display = "none";
+    }
+    else{
+        document.getElementById("txtbox")
+    }
+}*/
